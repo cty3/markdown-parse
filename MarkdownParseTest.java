b946bbe74d40904ea9a23b4b5d096193caa55b0e
cty3 <97644394+cty3@users.noreply.github.com>
2022-02-03 10:18:37 -0800

    Copy group member's codes

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,43 +1,49 @@
 import static org.junit.Assert.*;
-import org.junit.*;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+
+import org.junit.*;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+
     @Test
-    public void testGetLinks1() throws IOException{
-        Path fileName = Path.of("test-file.md");
-	    String contents = Files.readString(fileName);
+    public void testGetLinks() throws IOException {
+        Path filename = Path.of("test-file2.md");
+        String contents = Files.readString(filename);
+        assertEquals(MarkdownParse.getLinks(contents), List.of("https://something.com", "some-page.html"));
+
+        filename = Path.of("test-file.md");
+        contents = Files.readString(filename);
+        assertEquals(MarkdownParse.getLinks(contents), List.of("https://something.com", "some-page.html", "hello"));
+
+        filename = Path.of("test-file3.md");
+        contents = Files.readString(filename);
+        assertEquals(MarkdownParse.getLinks(contents), List.of("https://something.com", "some-page.html"));
+
+        filename = Path.of("test-file4.md");
+        contents = Files.readString(filename);
+        assertEquals(MarkdownParse.getLinks(contents), List.of("https://something.com", "some-page.html"));
+    }
+
+    /*
+    @Test
+    public void testGetLinks() {
         boolean exceptionThrown = false;
-        try{
-            MarkdownParse.getLinks(contents);
-        }catch(IOException e){
+        Path filename = Path.of("test-file2.md");
+        String contents;
+        try {
+            contents = Files.readString(filename);
+        } catch (IOException e) {
             exceptionThrown = true;
         }
-        assertTrue("IOException not thrown for test-file.md", exceptionThrown);
+        assertEquals(false, exceptionThrown);
     }
-    @Test
-    public void testGetLinks2() throws IOException{
-        Path fileName = Path.of("test-file-2.md");
-	    String contents = Files.readString(fileName);
-        assertEquals(MarkdownParse.getLinks(contents), List.of("https://something.com", "some-page.html", "hello"));
-    }
-    @Test
-    public void testGetLinks3() throws IOException{
-        Path fileName = Path.of("test-file-2.md");
-	    String contents = Files.readString(fileName);
-        assertEquals(MarkdownParse.getLinks(contents), List.of("https://something.com", "some-page.html", "hello"));
-    }
-    @Test
-    public void testGetLinks4() throws IOException{
-        Path fileName = Path.of("test-file-2.md");
-	    String contents = Files.readString(fileName);
-        assertEquals(MarkdownParse.getLinks(contents), List.of("https://something.com", "some-page.html", "hello"));
-    }
+    */
 }
